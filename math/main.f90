e3c220a6586c6e5e3bc46e24a3940a4f258ba0e6
黄德平(Deping Huang, Macbook Air) <xiaohengdao@gmail.com>
2020-09-09 21:14:17 +0800

    add 17_laplacian

--- math/main.f90
+++ math/main.f90
@@ -1,20 +1,9 @@
 program main
     use module_common
     implicit none
-    real(8)                :: a(n,n)
-    real(8)                :: b(n,n)
-    real(8)                :: x1
-    integer                :: m
 
-
-    call random_seed()
-    do i = 1,n
-        do j = 1,n
-            call random_number(x1)
-            a(i,j) = 10*x1
-        end do
-    end do
-    m = 10
-    b = power_mat(a,n,m)
+    !print *,dlog(1.0)
+    !print *,log(1D0)
+    !print *,log(1.0)
 
 end program main
