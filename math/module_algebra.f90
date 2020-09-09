dd7f4ec5f11f2a715a26e5924a81d806bb6368a9
黄德平(Deping Huang, Macbook Air) <xiaohengdao@gmail.com>
2020-09-09 21:14:36 +0800

    add 28_solveLinear

--- math/module_algebra.f90
+++ math/module_algebra.f90
@@ -61,17 +61,16 @@ function sol_equ(a,b,n)
      end do  !i
 end function sol_equ
 !}}}
-!  get Ax=b  A(n,n+1)
-!   
-!function sol_equ2{{{
-function sol_equ2(a,b,n)
+! get AX=B (A, B, X for matrix)
+!function sol_mat{{{
+function sol_mat(a,b,n)
     ! in and out dummies
     integer,intent(in)    :: n
-    real(8),intent(in)    :: a(n,n+1)
-    real(8),intent(in)    :: b(n)
-    real(8)               :: sol_equ2(n,2)
+    real(8),intent(in)    :: a(n,n)
+    real(8),intent(in)    :: b(n,n)
+    real(8)               :: sol_mat(n,n)
 
-    real(8)               :: c(n,n+2)
+    real(8)               :: c(n,n*2)
     real(8)               :: tmp
     real(8)               :: rate
     integer               :: s
@@ -79,20 +78,15 @@ function sol_equ2(a,b,n)
     integer               :: j
     integer               :: k
     s = 0
-    ! initial the new array c(n,n+1)
+    ! initial the new array c(n,n*2)
     do i = 1,n
-        do j = 1,n+1
-            c(i,j) = a(i,j)
+        do j = 1,n
+            c(i,j)   = a(i,j)
+            c(i,n+j) = b(i,j)
         end do
-        c(i,n+2) = b(i)
     end do
-    ! print the input data
-    !print *,"n = 4"
     !do i = 1,n
-    !    print "(6f18.6)",c(i,:) 
-    !end do
-    !do i = 1,n
-    !    print "(<n+1>f8.3)",c(i,:)
+    !    print "(<n*2>f8.3)",c(i,:)
     !end do
     
      do  i = 1,n
@@ -104,7 +98,7 @@ function sol_equ2(a,b,n)
                 s = 1
                 cycle
             else
-                do k = i,n+2          !  the ith column to  (n+2)th  column
+                do k = i,n*2          !  the ith column to  (n*2)th  column
                       tmp = c(i,k)
                    c(i,k) = c(j,k)
                    c(j,k) = tmp
@@ -116,20 +110,96 @@ function sol_equ2(a,b,n)
                 if(j == i)cycle
                 if(c(j,i) == 0)cycle
                 rate = c(j,i)/c(i,i)
-                do k = i,n+2         !  the ith column to  (n+2)th  column
+                do k = i,n*2         !  the ith column to  (n*2)th  column
                     c(j,k) = c(j,k)-rate*c(i,k)
                 end do   !k
             end do   !j
          end if     
-         !do j = 1,n
-         !    print "(6f18.6)",c(j,:)
-         !end do
-         !pause
      end do   !i
      do i = 1,n
-         sol_equ2(i,1) = c(i,n+2)/c(i,i)
-         sol_equ2(i,2) = - c(i,n+1)/c(i,i)
+        do j = 1,n
+          sol_mat(i,j) = c(i,n+j)/c(i,i)
+        end do
      end do  !i
-end function sol_equ2
+end function sol_mat
+!}}}
+!function multi_mat{{{
+function multi_mat(a,b,nlen,mlen,llen)
+    integer,intent(in)        :: nlen
+    integer,intent(in)        :: mlen
+    integer,intent(in)        :: llen
+    real(8),intent(in)        :: a(nlen,mlen)
+    real(8),intent(in)        :: b(mlen,llen)
+    real(8)                   :: multi_mat(nlen,llen)
+    integer                   :: ii
+    integer                   :: jj
+    integer                   :: kk
+
+    do ii = 1,nlen
+        do jj = 1,llen
+            ! get the value of every element
+            multi_mat(ii,jj) = 0.0
+            do kk = 1,mlen
+                multi_mat(ii,jj) =  multi_mat(ii,jj) &
+                                    + a(ii,kk)*b(kk,jj)
+            end do
+        end do
+    end do
+end function multi_mat
+!}}}
+!function multi_vec{{{
+function multi_vec(a,b,nlen,mlen)
+    integer,intent(in)        :: nlen
+    integer,intent(in)        :: mlen
+    real(8),intent(in)        :: a(nlen,mlen)
+    real(8),intent(in)        :: b(mlen)
+    real(8)                   :: multi_vec(nlen)
+    integer                   :: ii
+    integer                   :: kk
+
+    do ii = 1,nlen
+        ! get the value of every element
+        multi_vec(ii) = 0.0
+        do kk = 1,mlen
+            multi_vec(ii) =  multi_vec(ii) &
+                             + a(ii,kk)*b(kk)
+        end do
+    end do
+end function multi_vec
+!}}}
+!function mat_add{{{
+function mat_add(a,b,nlen,mlen)
+    integer,intent(in)        :: nlen
+    integer,intent(in)        :: mlen
+    real(8),intent(in)        :: a(nlen,mlen)
+    real(8),intent(in)        :: b(nlen,mlen)
+    real(8)                   :: mat_add(nlen,mlen)
+    integer                   :: ii
+    integer                   :: kk
+
+    do ii = 1,nlen
+        do kk = 1,mlen
+            mat_add(ii,kk) =  a(ii,kk) + b(ii,kk) 
+        end do
+    end do
+end function mat_add
+!}}}
+!function vec_multi_add{{{
+! x(n) = a(n,n)*x(n) + b(n) 
+function vec_multi_add(a,x,b,nlen)
+    integer,intent(in)        :: nlen
+    real(8),intent(in)        :: a(nlen,nlen)
+    real(8),intent(in)        :: b(nlen)
+    real(8),intent(in)        :: x(nlen)
+    real(8)                   :: vec_multi_add(nlen)
+    real(8)                   :: multi(nlen)
+    integer                   :: ii
+    integer                   :: kk
+
+    multi = multi_vec(a,x,nlen,nlen)
+    do ii = 1,nlen
+        vec_multi_add(ii) = multi(ii) + b(ii)
+    end do
+end function vec_multi_add 
 !}}}
 end module module_algebra
