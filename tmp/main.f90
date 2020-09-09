0a01ad31ba984d3df8f8ca2462e4f76b0d5e5325
黄德平(Deping Huang, Macbook Air) <xiaohengdao@gmail.com>
2020-09-09 21:17:32 +0800

    add tmp/02_v2/03_v3

--- tmp/main.f90
+++ tmp/main.f90
@@ -1,14 +1,16 @@
 program main
-    use module_common
     implicit none
-
+    integer,parameter         :: n = 10
+    integer   i,j,k
 
     do i = 1,n
-        a(i) = i
-    end do
-
-    print *,a(200000:200010)
-
-    print *,sign(200)
-    
+        do j = 1,n 
+            do k = 1,n
+                print *,i+j+k
+            enddo !cycle ends
+             
+        enddo !cycle ends
+         
+    enddo !cycle ends
+     
 end program main
