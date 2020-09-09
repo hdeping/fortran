913122027c71d69de89122fc476a7cd85e0999c8
黄德平(Deping Huang, Macbook Air) <xiaohengdao@gmail.com>
2020-09-09 21:49:41 +0800

    add 04_NewMeanAngle

--- before/04_ViscekModel/main.f90
+++ before/04_ViscekModel/main.f90
@@ -7,35 +7,38 @@ program main
     filename = "data.txt"
     open(10,file = filename)
     call random_seed()
-    ! initialize the configuration
-    call init_config()
-    vMean = getv(angle,num)
-    !print *,"mean velocity is ",vMean
-    !pause
-    do j = 1,50
-        !j   = 10
+    !do j = 1,50
+        j   = 10
         eta = 1D-1 * dble(j)
+        !eta = 0D0
+        ! initialize the configuration
+        call init_config()
         icount = 0
         vvalue = 0D0
+        veloMeanTime = 0D0
         call cpu_time(t1)
         do i = 1,n
             call update()
             if ( mod(i,fre) == 0 )then
                 icount = icount + 1
-                !  get the time average of the angle
-                if ( icount == 1 )then
-                    angleMean = angle
-                else
-                    angleMean = (angleMean*(icount - 1) + angle)/dble(icount)
-                endif ! if ends
-                vMean  = getv(angleMean,num)
-                !vvalue = (vvalue*(icount - 1) + vMean)/dble(icount)
+                vMean  = getv(velo)
+                vvalue = (vvalue*(icount - 1) + vMean)/dble(icount)
+                do k = 1,num
+                    do kk = 1,2
+                        veloMeanTime(k,kk) = (veloMeanTime(k,kk)*&
+                                (icount - 1) + velo(k,kk))/dble(icount)
+                    end do
+                end do
+                print *,"mean velocity is ",vvalue
+                pause
             endif ! if ends
         enddo !cycle ends
-        write(10,*)eta,vMean
-        print *,"mean velocity is ",vMean
+        call getNormVelo(veloMeanTime)
+        vMean = getv(veloMeanTime)
+        write(10,*)eta,vvalue,vMean
+        !write(10,*)eta,vMean
         call cpu_time(t2)
         print *,"time cost is ",t2 - t1
-    enddo !cycle ends
+    !enddo !cycle ends
     close(10)
 end program main
--- before/04_ViscekModel/module_common.f90
+++ before/04_ViscekModel/module_common.f90
@@ -1,6 +1,6 @@
 module module_common
     implicit none
-    integer,parameter            :: n   = 100000      ! times of evolution
+    integer,parameter            :: n   = 10000      ! times of evolution
     integer,parameter            :: num = 40          ! number of particles
     integer,parameter            :: fre = 100         ! frequence of printing
     integer,parameter            :: nl  = 7           ! 
@@ -11,8 +11,10 @@ module module_common
     real(8),parameter            :: r   = 1D0         ! interation radius
     real(8)                      :: eta               ! noise
     real(8)                      :: coor(num,2)       ! position
+    real(8)                      :: velo(num,2)       ! velo
     real(8)                      :: angle(num)        ! noise
     real(8)                      :: vMean             ! mean velocity
+    real(8)                      :: veloMeanTime(num,2)! mean velocities
     real(8)                      :: angleMean(num)    ! mean velocity
     real(8)                      :: t1
     real(8)                      :: t2
@@ -20,6 +22,7 @@ module module_common
     integer                      :: i
     integer                      :: j
     integer                      :: k
+    integer                      :: kk
     character(10)                :: filename 
     contains
 !subroutine init_config{{{
@@ -40,7 +43,9 @@ subroutine init_config()
     ! initialize the directions (0,2*pi)
     do ii = 1,num
         call random_number(x1)
-        angle(ii) = 2*pi*x1
+        angle(ii)  = 2*pi*x1
+        velo(ii,1) = v*cos(angle(ii))
+        velo(ii,2) = v*sin(angle(ii))
     enddo !cycle ends
 end subroutine init_config
 !}}}
@@ -50,27 +55,34 @@ subroutine update()
     integer                      :: jj
     real(8)                      :: delta_theta
     real(8)                      :: x1
-    real(8)                      :: meanAngle(num)
+    real(8)                      :: meanVelo(num,2)
 
     ! update the position
     do ii = 1,num
-        coor(ii,1) = coor(ii,1) + v*cos(angle(ii))
-        coor(ii,2) = coor(ii,2) + v*sin(angle(ii))
-        coor(:,1)  = mod(coor(:,1),l)
-        coor(:,2)  = mod(coor(:,2),l)
+        coor(ii,1) = coor(ii,1) + velo(ii,1)
+        coor(ii,2) = coor(ii,2) + velo(ii,2)
+        coor(ii,1)  = mod(coor(ii,1),l)
+        coor(ii,2)  = mod(coor(ii,2),l)
     enddo !cycle ends
     ! update the direction
-    ! get mean angle
-    meanAngle = getmean()
+    ! get mean velocity
+    meanVelo = getmean()
+    print *,"meanVelo"
+    print *,meanVelo(1,1),meanVelo(1,2)
+    print *,sqrt(meanVelo(1,2)**2.0 + meanVelo(1,2)**2.0)
+    pause
     do ii = 1,num
         call random_number(x1)
         delta_theta = eta*(x1 - 0.5)
-        angle(ii)   = meanAngle(ii) + delta_theta
-        angle(ii)   = mod(angle(ii),2.0*pi)
+        velo(ii,1) = cos(delta_theta)*velo(ii,1) - &
+                     sin(delta_theta)*velo(ii,2)
+        velo(ii,2) = sin(delta_theta)*velo(ii,1) + &
+                     cos(delta_theta)*velo(ii,2)
     enddo !cycle ends
 end subroutine update
 !}}}
 !function getmean{{{
+! get mean velocity
 function getmean()
     integer                      :: ii
     integer                      :: jj
@@ -79,7 +91,9 @@ function getmean()
     integer                      :: statu(num)! statu of particle
     integer                      :: times(num)! surrounding number with r 
     real(8)                      :: distance
-    real(8)                      :: getmean(num)
+    real(8)                      :: tmp
+    real(8)                      :: getmean(num,2)  
+    real(8)                      :: mean_velo(num,2)  ! mean velocity
 
     ! get statu of particles
     do ii = 1,num
@@ -100,11 +114,14 @@ function getmean()
        statu(ii) = 3*kk + jj
     enddo !cycle ends
     ! initialize the getmean and times
-    getmean = angle
-    times   = 1
+
+    do ii = 1,num
+        mean_velo(ii,1) = cos(angle(ii))
+        mean_velo(ii,2) = sin(angle(ii))
+    end do ! cycle ends
+    times      = 1
     ! get the distance between two particles 
-    ! get the meanAngle
-!{{{
+!get the velocity{{{
     do ii = 1,num - 1
         do jj = ii + 1,num 
             ! sort the condition 
@@ -179,19 +196,37 @@ function getmean()
             if ( distance < r )then
                 times(ii)   = times(ii) + 1
                 times(jj)   = times(jj) + 1
-                getmean(ii) = (getmean(ii)*(times(ii) - 1) + &
-                              angle(jj))/dble(times(ii))
-                getmean(jj) = (getmean(jj)*(times(jj) - 1) + &
-                              angle(ii))/dble(times(jj))
+                mean_velo(ii,1) = (mean_velo(ii,1)*(times(ii) - 1) + &
+                                  cos(angle(jj)))/dble(times(ii))
+                mean_velo(ii,2) = (mean_velo(ii,2)*(times(ii) - 1) + &
+                                  sin(angle(jj)))/dble(times(ii))
+                mean_velo(jj,1) = (mean_velo(jj,1)*(times(jj) - 1) + &
+                                  cos(angle(jj)))/dble(times(jj))
+                mean_velo(jj,2) = (mean_velo(jj,2)*(times(jj) - 1) + &
+                                  sin(angle(ii)))/dble(times(jj))
                 !print *,ii,jj,"distance = ",distance
                 !print "(4f12.4)",coor(ii,1),coor(ii,2),coor(jj,1),coor(jj,2)
                 !print *,statu(ii),statu(jj),mark
                 !pause
             endif ! if ends
-
         enddo !cycle ends
     enddo !cycle ends
 !}}}
+    call getNormVelo(mean_velo)
+    getmean = mean_velo
+    !! get the meanAngle
+    !do ii = 1,num
+    !    tmp         = sqrt(mean_velo(ii,1)**2.0 + mean_velo(ii,2)**2.0) 
+    !    getmean(ii) = asin(mean_velo(ii,1)/tmp)
+    !    if (  mean_velo(ii,1) < 0D0)then
+    !        if     ( mean_velo(ii,2) > 0D0)then
+    !            getmean(ii) = pi - getmean(ii)
+    !        elseif ( mean_velo(ii,2) < 0D0)then
+    !            getmean(ii) = - pi - getmean(ii)
+    !        endif ! if ends
+    !    endif ! if ends
+    !end do ! ii
+    
      
 end function getmean
 !}}}
@@ -199,21 +234,30 @@ end function getmean
 ! get the mean velocity
 ! a for angle
 ! n for the number of particles
-function getv(a,n)
-    integer,intent(in)           :: n
-    real(8),intent(in)           :: a(n)
-    integer                      :: ii
+function getv(velo)
+    real(8),intent(in)           :: velo(num,2)
     real(8)                      :: getv
     real(8)                      :: xtmp
     real(8)                      :: ytmp
 
-    xtmp  = 0D0
-    ytmp  = 0D0
-    do ii = 1,n
-        xtmp = xtmp + cos(a(ii))
-        ytmp = ytmp + sin(a(ii))
-    enddo !cycle ends
-    getv = sqrt(xtmp**2.0 + ytmp**2.0)/dble(n) 
+    xtmp = sum(velo(:,1))
+    ytmp = sum(velo(:,2))
+    getv = sqrt(xtmp**2.0 + ytmp**2.0)/(v*dble(num))
 end function getv
+!}}}
+!subroutine getNormVelo{{{
+subroutine getNormVelo(velo)
+    real(8),intent(inout)          :: velo(num,2)
+    integer                        :: ii
+    integer                        :: jj
+    real(8)                        :: tmp
+
+    do ii = 1,num
+       tmp = v/sqrt(velo(ii,1)**2.0 + velo(ii,2)**2.0)
+       do jj = 1,2
+           velo(ii,jj) = velo(ii,jj)*tmp
+       end do
+    end do
+end subroutine getNormVelo
 !}}}
 end module module_common
