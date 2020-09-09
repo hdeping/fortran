9a6cbecb4b0cc281bf4116b59c15aa988bc3aab6
黄德平(Deping Huang, Macbook Air) <xiaohengdao@gmail.com>
2020-09-09 21:13:35 +0800

    add 07_Riemman_postu

--- math/main.f90
+++ math/main.f90
@@ -1,38 +1,40 @@
 program main
-    use lapack95
+    use module_common
     implicit none
-    integer,parameter      :: n = 4
-    real(8)                :: a(n,n)
-    real(8)                :: b(n)
-    real(8)                :: x(n)
-    real(8)                :: t1
-    real(8)                :: t2
-    real(8)                :: x1
-    integer                :: i
-    integer                :: j
-    integer                :: ii
-    integer                :: jj
+    integer             :: i 
+    integer             :: j 
+    character(10)       :: filename 
+    
+    filename = "data.txt"
+    open(10,file = filename)
 
-    !call random_seed()
-    !do ii = 1,n
-    !    do jj = 1,n
-    !        call random_number(x1)
-    !        a(ii,jj) = x1*10000.0 - 5000.0
-    !        if(ii == jj)a(ii,jj) = 0.0
-    !    end do
-    !    call random_number(x1)
-    !    b(ii) = 10000*x1 - 5000.0
-    !end do
-!program before{{{
-    a(1,:) = (/0.0,0.0,2.0,3.0/)
-    a(2,:) = (/5.0,4.0,0.0,0.0/)
-    a(3,:) = (/3.0,0.0,3.0,0.0/)
-    a(4,:) = (/0.0,3.0,0.0,9.0/)
-!}}}
+    call random_seed()
+    ! test the functions
+    !a   = 1.0
+    !b   = 1.0
+    !call cpu_time(t1)
+    !x   = getzero(a,b)
+    !call cpu_time(t2)
+    !print *,"time cost is ==> ",t2 - t1
+    !print *,"x = ",x(:)
+    !print *,"f(a,b) = ",fab(x(1),x(2))
+    !print *,"g(a,b) = ",gab(x(1),x(2))
+    
+    !a = 1.0
+    !b = 1.0
+    !x = getzero(a,b)
+    !print *,"x = ",x(:)
+    do i = 1,100000
+       call random_number(x1)
+       call random_number(x2)
+       x1 = 1000*x1 + 1
+       x2 = 2*pi*x2
+       a  = x1*cos(x2)
+       b  = x1*sin(x2)
+       x  = getzero(a,b)
+       if(lambda > 100)cycle
+       write(10,"(2f12.4)")x(:)
+    end do
 
-    call cpu_time(t1)
-    !x = iter_sol_equ(a,b,n) 
-    print *,"det = ",x1
-    call cpu_time(t2)
-    print *,"time cost is ==>  ",t2 - t1
+    close(10)
 end program main
