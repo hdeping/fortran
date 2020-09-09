b7da15801e6fc86578c4054184372c6ad57906e3
黄德平(Deping Huang, Macbook Air) <xiaohengdao@gmail.com>
2020-09-09 21:35:49 +0800

    add ../1_fft_huang

--- before/07_oz_equation/5_test/3_fft_jiang/main.f90
+++ before/07_oz_equation/5_test/3_fft_jiang/main.f90
@@ -1,16 +1,23 @@
 program main
     use module_common
-    use module_fst
+    use module_fft
     real(8)     a(n)
     real(8)     b(n)
     real(8)     c(n)
+    real        t1
+    real        t2
 
+    call cpu_time(t1)
     do i = 1,n
         a(i) = i
     end do
-    call fst(b,a,n,l,1)
+    call fft(b,a,n,l,1)
     ! print 
-    call fst(c,b,n,l,-1)
+    call fft(c,b,n,l,-1)
+    call cpu_time(t1)
+
+    print *,"time cost is ",t2 - t1
+
     !write(*,*)"a     b      c"
     !do i = 1,n
     !    write(*,"(3f10.5)")a(i),b(i),c(i)
