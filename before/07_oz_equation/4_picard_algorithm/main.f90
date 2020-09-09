a0c30c24d8bc30dbc71804c168dbd36eab238795
黄德平(Deping Huang, Macbook Air) <xiaohengdao@gmail.com>
2020-09-09 21:44:53 +0800

    add 08_PRISM_theory/01_ver_norm

--- before/07_oz_equation/4_picard_algorithm/main.f90
+++ before/07_oz_equation/4_picard_algorithm/main.f90
@@ -2,12 +2,13 @@ program main
     !use module_fst 
     use module_common
     use module_evolution
-    integer               :: itmp
     call random_seed()
 
 !get the solution of OZ{{{
     filename = "gr.txt"
-    open(10,file = filename)
+    open(20,file = filename)
+    filename = "sk.txt"
+    open(30,file = filename)
 
     !  initial the k and r
     do i = 1,n
@@ -15,26 +16,45 @@ program main
         dr(i) = (i - 1)*deltar
     end do
     !  calculate the mayer function
-    may11 = may(d11)
-    may12 = may(d12)
-    may22 = may(d22)
+    maypp = may(dpp)
+    maywp = may(dwp)
+    mayww = may(dww)
+    maynp = may(dnp)
+    maynw = may(dnw)
 
     call cpu_time(t1)
 !*********** solve the equation **********
     call evolution()
+!*********** nano-particle **********
+    call evolution_nano()
 !*****************************************
-    call cpu_time(t2)
-    print *,"time cost is ",t2 - t1
+    !call check()
+    !lambda = judge(test,ckpp)
+    !lambda1 = judge(test1,ckwp)
+    !lambda2 = judge(test2,ckww)
+    !print *,lambda,lambda1,lambda2
 
-    !  write gr to the file
-            do itmp = 2,n
-                gr11(itmp) = (cr11(itmp) + gamma_r11(itmp))/dr(itmp) + 1.0
-                gr12(itmp) = (cr12(itmp) + gamma_r12(itmp))/dr(itmp) + 1.0
-                gr22(itmp) = (cr22(itmp) + gamma_r22(itmp))/dr(itmp) + 1.0
-            end do
-    do ii = 2,n
-        write(10,"(4f12.5)")dr(ii),gr11(ii),gr12(ii),gr22(ii)
+    call cpu_time(t2)
+
+    print *,"lambda is ",lambda
+    !  write ck to the file
+     grpp(1) = 0.0 
+     grwp(1) = 0.0 
+     grpp(1) = 0.0 
+    do i = 2,n
+       grpp(i) = (crpp(i)  +  gamma_rpp(i) )/dr(i)  + 1
+       grwp(i) = (crwp(i)  +  gamma_rwp(i) )/dr(i)  + 1
+       grww(i) = (crww(i)  +  gamma_rww(i) )/dr(i)  + 1
+       grnp(i) = (crnp(i)  +  gamma_rnp(i) )/dr(i)  + 1
+       grnw(i) = (crnw(i)  +  gamma_rnw(i) )/dr(i)  + 1
+    end do   !  i
+    do i = 1,n
+        write(20,"(6f10.5)")dr(i),grpp(i),grwp(i),grww(i),grnp(i),grnw(i)
     end do
-    close(10)
+
+    ! get sk
+
+    close(30)
+    close(20)
 !}}}
 end program main
--- before/07_oz_equation/4_picard_algorithm/module_common.f90
+++ before/07_oz_equation/4_picard_algorithm/module_common.f90
@@ -4,21 +4,26 @@ module module_common
 
     implicit none 
 !parameters{{{
-    integer,parameter           ::   l    = 10
+    integer,parameter           ::   l    = 12
     integer,parameter           ::   n    = 2**l
-    integer,parameter           ::   m    = 2
     integer,parameter           ::  fre   =  int(1E2)
     real(8),parameter           ::   pi   = 3.141592653 
-    real(8),parameter           :: deltar = 0.01
+    real(8),parameter           :: deltar = 0.04 
     real(8),parameter           :: deltak = pi/dble(n)/deltar 
     real(8),parameter           :: error  = 1E-8      !  for the diwwerences
     ! the diameter
-    real(8),parameter           :: d11    = 1.0       !  
-    real(8),parameter           :: d22    = 1.0      !  
-    real(8),parameter           :: d12    = (d11 + d22)/2.0  
+    real(8),parameter           :: dpp    = 1.0       !  
+    real(8),parameter           :: dww    = 1.0      !  
+    real(8),parameter           :: dwp    = (dpp + dww)/2.0  
+    real(8),parameter           :: dnp    = 1.0       !  
+    real(8),parameter           :: dnw    = 1.0       !  
     ! the number dendity
-    real(8),parameter           :: rho1   = 0.01
-    real(8),parameter           :: rho2   = 0.1
+    real(8),parameter           :: rhop   = 0.4
+    real(8),parameter           :: rhow   = 0.4
+    integer,parameter           :: ntmp   = 3
+    ! prism
+    real(8),parameter           ::  b0    = 1.269*dpp    ! length of bond
+    integer,parameter           ::  nnum  = 50 ! number of polymer monomer 
 !}}}
 !common variables{{{
     real(8)                     ::  k
@@ -29,20 +34,27 @@ module module_common
     real(8)                     ::  t2
     real(8)                     ::  eta
     real(8)                     ::  xtmp
+    real(8)                     ::  a(ntmp,ntmp)
+    real(8)                     ::  b(ntmp)
+    real(8)                     ::  bnew(ntmp)
+    real(8)                     ::  x(ntmp)
     real(8)                     ::  x1
-    real(8)                     ::  rate
+    real                        ::  rate
     ! judge the convergence
-    real(8)                     ::  lambda
-    real(8)                     ::  lambda1
-    real(8)                     ::  test(m,m,n)   
+    real(8)                     ::  lambda_test
+    real(8)                     ::  lambda    
+    real(8)                     ::  lambda_final
+    real(8)                     ::  lambda1  
+    real(8)                     ::  lambda2  
+    real(8)                     ::  test(n)   
     real(8)                     ::  test1(n)   
     real(8)                     ::  test2(n)   
-    real(8)                     ::  test3(n)   
     ! mayer function  
-    real(8)                     ::  mayfun(m,m,n)  ! p-p
-    real(8)                     ::  may11(n)
-    real(8)                     ::  may12(n)
-    real(8)                     ::  may22(n)
+    real(8)                     ::  maypp(n)  ! p-p
+    real(8)                     ::  maywp(n) ! w-p
+    real(8)                     ::  mayww(n)  ! w-w
+    real(8)                     ::  maynp(n) ! w-p
+    real(8)                     ::  maynw(n)  ! w-w
     integer                     ::  i        
     integer                     ::  j        
     integer                     ::  times        
@@ -51,36 +63,61 @@ module module_common
     ! variables for OZ equation  
 !OZ{{{
     ! for convenience, h for H, c for C
+    ! H = r*h, C = r*c
+    ! p for polymer
+    ! w for water
+    ! n for nano particle
     !  k space 
-    real(8)                     ::  hk11(n)
-    real(8)                     ::  hk12(n)
-    real(8)                     ::  hk22(n)
-    real(8)                     ::  ck11(n)
-    real(8)                     ::  ck12(n)
-    real(8)                     ::  ck22(n)
-    real(8)                     ::  gamma_k11(n)
-    real(8)                     ::  gamma_k12(n)
-    real(8)                     ::  gamma_k22(n)
+    real(8)                     ::  hkpp(n)      
+    real(8)                     ::  hkwp(n)      
+    real(8)                     ::  hkww(n)      
+    real(8)                     ::  hknw(n)      
+    real(8)                     ::  hknp(n)      
+
+    real(8)                     ::  gamma_kpp(n)      
+    real(8)                     ::  gamma_kwp(n)      
+    real(8)                     ::  gamma_kww(n)      
+    real(8)                     ::  gamma_knw(n)      
+    real(8)                     ::  gamma_knp(n)      
+
+    real(8)                     ::  ckpp(n)      
+    real(8)                     ::  ckwp(n)      
+    real(8)                     ::  ckww(n)      
+    real(8)                     ::  cknw(n)      
+    real(8)                     ::  cknp(n)      
     !  r space                       
-    real(8)                     ::  hr11(n)
-    real(8)                     ::  hr12(n)
-    real(8)                     ::  hr22(n)
-    real(8)                     ::  cr11(n)
-    real(8)                     ::  cr12(n)
-    real(8)                     ::  cr22(n)
-    real(8)                     ::  gamma_r11(n)
-    real(8)                     ::  gamma_r12(n)
-    real(8)                     ::  gamma_r22(n)
+    real(8)                     ::  hrpp(n)      
+    real(8)                     ::  hrwp(n)      
+    real(8)                     ::  hrww(n)      
+    real(8)                     ::  hrnw(n)      
+    real(8)                     ::  hrnp(n)      
+
+    real(8)                     ::  gamma_rpp(n)      
+    real(8)                     ::  gamma_rwp(n)      
+    real(8)                     ::  gamma_rww(n)      
+    real(8)                     ::  gamma_rnw(n)      
+    real(8)                     ::  gamma_rnp(n)      
+
+    real(8)                     ::  crpp(n)      
+    real(8)                     ::  crwp(n)      
+    real(8)                     ::  crww(n)      
+    real(8)                     ::  crnw(n)      
+    real(8)                     ::  crnp(n)      
     ! g(r)
-    real(8)                     ::  gr11(n)
-    real(8)                     ::  gr12(n)
-    real(8)                     ::  gr22(n)
+    real(8)                     ::  grpp(n)      
+    real(8)                     ::  grwp(n)      
+    real(8)                     ::  grww(n)      
+    real(8)                     ::  grnw(n)      
+    real(8)                     ::  grnp(n)      
     ! structure factor
-    real(8)                     ::  sk11(n)
-    real(8)                     ::  sk12(n)
-    real(8)                     ::  sk22(n)
+    real(8)                     ::  skpp(n)      
+    real(8)                     ::  skwp(n)      
+    real(8)                     ::  skww(n)      
+    real(8)                     ::  sknw(n)      
+    real(8)                     ::  sknp(n)      
 !}}}
     ! variables of prism
+    real(8)                     ::  wp(n) ! partition function
 
 
 
