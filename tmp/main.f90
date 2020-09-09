78f9f2ab7e289afcd89b73e0bd3bc71e0417744f
黄德平(Deping Huang, Macbook Air) <xiaohengdao@gmail.com>
2020-09-09 21:17:13 +0800

    add tmp/02_v2

--- tmp/main.f90
+++ tmp/main.f90
@@ -1,44 +1,14 @@
 program main
+    use module_common
     implicit none
-    integer,parameter    :: n    = 48             ! months
-    integer,parameter    :: s    = 24000          ! total money
-    real,parameter       :: rate = 5E-3           ! rate
-    real                 :: x                     ! money every month
-    real                 :: y                     ! fluctuation
-    real                 :: y1                    ! random number
-    real                 :: s1                    ! 
-    real                 :: s2                    !
-    real                 :: tmp                   !
-    integer              :: i                     !  count
-    integer              :: j                     !
-    integer              :: num(100)
-    character(20)        :: filename
 
-    filename = "new.txt"
-    open(10,file = filename,status = "old")
 
-    call random_seed()
-    s1 = 4.056
-    s2 = 89.056
-    x  = s1/s2
+    do i = 1,n
+        a(i) = i
+    end do
 
-    write(10,*)"s1 = ",s1 
-    write(10,*)"s2 = ",s2
-    write(10,*)"x  = ",x
-    close(10)
-    do i = 1,100
-        num(i) = 100 + i
-    end do
-    do i = 1,100
-        write(filename,"('file',i3.3,'.txt')")i
-        open(num(i),file = filename,status = "old")
-    end do
-    do i  = 1,100
-        write(num(i),"(i8)")i
-    end do
-    do i = 1,100
-        close(num(i))
-    end do
-    
+    print *,a(200000:200010)
+
+    print *,sign(200)
     
 end program main
---
+++ tmp/module_common.f90
@@ -0,0 +1,9 @@
+module module_common
+    implicit none
+    integer,parameter            :: n = int(2E8)
+    integer                      :: i
+    integer                      :: j
+    integer                      :: k
+    integer                      :: a(n)
+    character(10)                :: filename 
+end module module_common
