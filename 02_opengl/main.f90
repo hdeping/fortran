d2dcb305bc7beb4f89aad968da6b782025d7b745
黄德平(Deping Huang, Macbook Air) <xiaohengdao@gmail.com>
2020-09-09 21:22:33 +0800

    add 02_opengl/03_learn/02_animationTry

--- 02_opengl/main.f90
+++ 02_opengl/main.f90
@@ -2,25 +2,14 @@ program main
     use module_common
     implicit none
 
-    real   coor(3)
-
-
-
-    call initializeAWindowPlease()
-    call glclearcolor(0.0,0.0,0.0,0.0)
-    call glclear(gl_color_buffer_bit)
-    call glcolor3f(1.0,1.0,1.0)
-    call glortho(0.0,1.0,0.0,1.0,- 1.0,1.0)
-    call glbegin(gl_polygon)
-        coor = (/0.25,0.75,0.00/)
-        glvertex3f(coor(1),coor(2),coor(3))
-        coor = (/0.75,0.25,0.00/)
-        glvertex3f(coor(1),coor(2),coor(3))
-        coor = (/0.75,0.75,0.00/)
-        glvertex3f(coor(1),coor(2),coor(3))
-        coor = (/0.25,0.75,0.00/)
-        glvertex3f(coor(1),coor(2),coor(3))
-    call glend()
-    call glflush()
-    call updateTheWindowAndCheckForEvents()
+    call glutinit_gl()
+    call glutinitdisplaymode(glut_double,glut_rgb)
+    call glutinitwindowsize(250,250)
+    call glutinitwindowposition(100,100)
+    call glutcreatewindow()
+    call init()
+    call glutdisplayfunc_gl(display)
+    call glutreshapefunc_gl(reshape)
+    call glutmousefunc(mouse)
+    call glutmainloop()
 end program main
