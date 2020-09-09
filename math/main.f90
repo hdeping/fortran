39667dc15b2c89f9e4b25ad19d0ac07c9473a195
黄德平(Deping Huang, Macbook Air) <xiaohengdao@gmail.com>
2020-09-09 21:14:15 +0800

    add 16_CayleyHamilton

--- math/main.f90
+++ math/main.f90
@@ -1,39 +1,20 @@
 program main
     use module_common
     implicit none
-    !real(8)          :: test(2*m,2*m)
-    real(8)          :: test(2*m)
-    
-    !*****************  get data
-    filename = "data.txt"
-    open(10,file = filename)
-    do i = 1,m
-        a_para(i) = dble(i)
-        b_para(i) = dble(m + 1 - i)
-    end do
-    do  i = 1,n
-        x(i) = dble(i)*dt
-    end do
-    y = funx(x,a_para,b_para)
+    real(8)                :: a(n,n)
+    real(8)                :: b(n,n)
+    real(8)                :: x1
+    integer                :: m
+
+
+    call random_seed()
     do i = 1,n
-        write(10,*)x(i),y(i)
+        do j = 1,n
+            call random_number(x1)
+            a(i,j) = 10*x1
+        end do
     end do
-    
-    close(10)
-    !  get parameter
-    !call getpara(anew,bnew,x,y)
-    !print "('a = ',<m>f18.5)",anew(:)
-    !print "('b = ',<m>f18.5)",bnew(:)
-
-
-    !  test 
-    !a_para = 3.0
-    test = equations(a_para,b_para,x,y)
-    print *,test
-    !test = getpartial(a_para,b_para,x,y)
-    
-
-
-    
+    m = 10
+    b = power_mat(a,n,m)
 
 end program main
