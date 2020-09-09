97d6ff2cc5e1196ff79808ec4d7b3d4ab65bb83e
黄德平(Deping Huang, Macbook Air) <xiaohengdao@gmail.com>
2020-09-09 21:14:25 +0800

    add 21_PhysConst

--- math/main.f90
+++ math/main.f90
@@ -1,19 +1,30 @@
 program main
     use module_common
     implicit none
-    
-    do i = 1,m
-        x(i) = 1D-2*dble(i)
-    enddo !cycle ends
-    y = laguerre(x) 
-    do i = 1,m
-        write(filename,"('laguerre',i3.3,'.txt')")i
-        !write(filename,"('legendre',i3.3,'.txt')")i
-        open(10,file = filename)
-        do j = 1,n
-            write(10,*)j,y(j,i)
-        enddo !cycle ends
-        close(10) 
-    enddo !cycle ends
-     
+    real(8)     :: s
+    real(8)     :: tmp
+    filename = "data.txt"
+    open(10,file = filename)
+
+    r = 5.3D-11
+    v = sqrt(ev**2.0/(4.0*pi*epsi0*emass*r))
+    print *,"v = ",v
+    s = ev**2.0/(4.0*pi*epsi0*emass*r*c**2.0)
+    print *,"s = ",s
+    do i = 1,118000
+        tmp = dble(i)*s
+        v   = sqrt(tmp)
+        tmp = tmp**2.0
+        tmp = (- tmp + sqrt(tmp**2.0 + 4*tmp))/2.0
+        x   = sqrt(tmp)
+        if ( mod(i,1000) == 0 )then
+            write(10,*)i,v,x
+        endif ! if ends
+        !print *,"i = ",i,"x = ",x
+    end do
+    close(10)
+    !s = g*emass**2.0/(r**2.0)
+    !print *,"force is ",s
+    !v = sqrt(13.6*ev*r/emass)
+    !print *,"v = ",v
 end program main
--- math/module_common.f90
+++ math/module_common.f90
@@ -1,61 +1,18 @@
 module module_common
     implicit none
-    integer,parameter            :: n = 100
-    integer,parameter            :: m = 100
+    integer,parameter            :: n     = 100
+    real(8),parameter            :: epsi0 = 8.85D-12
+    real(8),parameter            :: ev    = 1.6D-19
+    real(8),parameter            :: emass = 9.1D-31
+    real(8),parameter            :: h     = 6.626D-34
+    real(8),parameter            :: pi    = 3.1415927
+    real(8),parameter            :: c     = 2.98D8
+    real(8),parameter            :: g     = 6.61D-11
     integer                      :: i
     integer                      :: j
     integer                      :: k
-    real(8)                      :: x(m)
-    real(8)                      :: y(n,m)
-    character(20)                :: filename 
-
-    contains
-!function legendre{{{
-function legendre(x)
-    real(8),intent(in)    :: x(m)
-    real(8)               :: legendre(n,m)
-    integer               :: ii
-    integer               :: jj
-    integer               :: kk
-
-    do ii = 1,m
-        legendre(1,ii) = 1.0
-        legendre(2,ii) = x(ii)
-    enddo !cycle ends
-
-    do ii = 2,n - 1
-        do jj = 1,m
-            legendre(ii + 1,jj) =  legendre(ii,jj)*dble((2*jj+1))&
-                *x(jj)/dble((jj+1)) - legendre(ii - 1,jj)*dble(jj)&
-                /dble((jj+1))
-        enddo !cycle ends
-    enddo !cycle ends
-     
-     
-end function legendre
-!}}}
-!function laguerre{{{
-function laguerre(x)
-    real(8),intent(in)    :: x(m)
-    real(8)               :: laguerre(n,m)
-    integer               :: ii
-    integer               :: jj
-    integer               :: kk
-
-    do ii = 1,m
-        laguerre(1,ii) = 1.0
-        laguerre(2,ii) = 1.0 - x(ii)
-    enddo !cycle ends
-
-    do ii = 2,n - 1
-        do jj = 1,m
-            laguerre(ii + 1,jj) =  laguerre(ii,jj)*dble((2*jj+1 - x(jj)))&
-                /dble((jj+1)) - laguerre(ii - 1,jj)*dble(jj)&
-                /dble((jj+1))
-        enddo !cycle ends
-    enddo !cycle ends
-     
-     
-end function laguerre
-!}}}
+    real(8)                      :: r
+    real(8)                      :: v
+    real(8)                      :: x
+    character(10)                :: filename 
 end module module_common
