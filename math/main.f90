0b3bba17e66d5177dc5348db4f42eb4e3e98176f
黄德平(Deping Huang, Macbook Air) <xiaohengdao@gmail.com>
2020-09-09 21:13:37 +0800

    add 08_fivePointConical/01_version

--- math/main.f90
+++ math/main.f90
@@ -1,40 +1,45 @@
+! In general, a conical section
+! can be given by five points(for 2D)
+! In a n dimmesional space, the number 
+! of the points are n*(n+3)/2
 program main
     use module_common
+    use module_algebra
     implicit none
-    integer             :: i 
-    integer             :: j 
-    character(10)       :: filename 
     
+    !  read data
     filename = "data.txt"
-    open(10,file = filename)
-
-    call random_seed()
-    ! test the functions
-    !a   = 1.0
-    !b   = 1.0
-    !call cpu_time(t1)
-    !x   = getzero(a,b)
-    !call cpu_time(t2)
-    !print *,"time cost is ==> ",t2 - t1
-    !print *,"x = ",x(:)
-    !print *,"f(a,b) = ",fab(x(1),x(2))
-    !print *,"g(a,b) = ",gab(x(1),x(2))
-    
-    !a = 1.0
-    !b = 1.0
-    !x = getzero(a,b)
-    !print *,"x = ",x(:)
-    do i = 1,100000
-       call random_number(x1)
-       call random_number(x2)
-       x1 = 1000*x1 + 1
-       x2 = 2*pi*x2
-       a  = x1*cos(x2)
-       b  = x1*sin(x2)
-       x  = getzero(a,b)
-       if(lambda > 100)cycle
-       write(10,"(2f12.4)")x(:)
+    open(10,file = filename,status = "old",iostat = ierror)
+    do i = 1,m
+        read(10,*,iostat = ierror)x(i,:)
+        if(ierror /= 0)exit
     end do
 
+    !  get a and b
+    b = 1.0
+
+    do i = 1,m
+        num = 0
+        do j = 1,d
+            do k = j,d
+                num = num + 1
+                a(i,num) = x(i,j)*x(i,k)
+            end do
+        end do
+        num = num + 1
+        do j = num, m
+            a(i,j) = x(i,j - num)
+        end do
+    end do
+    y = sol_equ(a,b,m)
+    ! print the coefficients
+    print *,"y = "
+    do i = 1,m
+        print *,y(i)
+    end do
+    print *,"x = "
+    do i = 1,m
+        print *,x(i,:)
+    end do
     close(10)
 end program main
