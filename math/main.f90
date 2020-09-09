506c6d8f8bbe04fa86f0407b95c6a0d36cd4f6e8
黄德平(Deping Huang, Macbook Air) <xiaohengdao@gmail.com>
2020-09-09 21:14:42 +0800

    add 31_BaiduQuestion

--- math/main.f90
+++ math/main.f90
@@ -1,21 +1,30 @@
 program main
     use module_common
     implicit none
+    integer         :: times = 0
 
-    integer               :: n
+    filename = "data.txt"
+    open(10,file = filename)
 
-    ! initial the status
+    write(10,*)"times"
 
-    do n = 10,50
-         allocate(a(n))
-         allocate(b(n))
-         a = 0
-         c = getFinal(n,m)
-         print *,n,c
-         deallocate(a)
-         deallocate(b)
+    do i = 1,n - 4
+        do j = i + 1,n - 3
+            do k = j + 1,n - 2
+                do kk = k+1,n - 1
+                    do jj = kk + 1,n
+                        if (  i+j+k+kk+jj < 100)then
+                            cycle
+                        endif ! if ends
+                        if ( tail(i,j,k,kk,jj) /= 0 )then
+                            times = times + 1
+                            write(10,"(8i6)")times,i,j,k,kk,jj
+                        endif ! if ends
+                    enddo !cycle ends
+                enddo !cycle ends
+            enddo !cycle ends
+        enddo !cycle ends
     enddo !cycle ends
-    ! game begins
+    
      
-
 end program main
--- math/module_common.f90
+++ math/module_common.f90
@@ -1,71 +1,44 @@
 module module_common
     implicit none
-    !integer,parameter            :: n = 42
-    integer,parameter            :: m = 2
+    integer,parameter            :: n = 49
     integer                      :: i
     integer                      :: j
     integer                      :: k
-    integer,allocatable          :: a(:)  ! denotes status 
-    integer,allocatable          :: b(:)  
-    integer                      :: c(m-1)
-    integer                      :: times ! denotes the cycle number
-    integer                      :: countNum
+    integer                      :: kk
+    integer                      :: jj
     character(10)                :: filename 
+
     contains
-!function getThrees{{{
-function getThrees(a,n)
-    integer,intent(in)       :: n
-    integer,intent(in)       :: a(n)
-    integer                 :: getThrees
-    integer                 :: ii
+!function tail{{{
+function tail(a,b,c,d,e)
+    integer,intent(in)        ::  a
+    integer,intent(in)        ::  b
+    integer,intent(in)        ::  c
+    integer,intent(in)        ::  d
+    integer,intent(in)        ::  e
+    integer                   ::  tail
+    integer                   ::  judge(5)
+    integer                   ::  ii
+    integer                   ::  num1
+    integer                   ::  num2
 
-    getThrees = 0
-    do ii = 1,n
-        if ( a(ii) == 3 )then
-            getThrees = getThrees + 1 
-        endif ! if ends
+    judge = (/a,b,c,d,e/)
+
+    num1 = 0
+    num2 = 0
+    do ii = 1,5
+        num1 = num1 + judge(ii)
+        num2 = num2 + mod(judge(ii),10)
     enddo !cycle ends
-end function getThrees
+    if ( mod(num1,100) == num2 )then
+        tail = num2
+    else
+        tail = 0
+    endif ! if ends
+
+
+    
+
+end function tail
 !}}}
-!function getFinal{{{
-function getFinal(n,m)
-    integer,intent(in)     :: n
-    integer,intent(in)     :: m
-    integer                :: getFinal(m - 1)
-
-    a = 0
-    times = 0
-    countNum = 0
-    do 
-        times = times + 1
-        if ( times > n )then
-            times = mod(times,n)
-        endif ! if ends
-        
-        if ( a(times) == m )then
-            cycle
-        endif ! if ends
-
-        k = k + 1
-        if ( k > m )then
-            k = mod(k,m) 
-        endif ! if ends
-        a(times) = k
-        if ( k == m )then
-            countNum = countNum + 1
-            b(countNum) = times
-        endif ! if ends
-        if (countNum == n)then
-            exit
-        endif ! if ends
-    enddo !cycle ends
-
-    do k = n+2-m,n
-        getFinal(k-n+m-1) = b(k)
-    enddo !cycle ends
-
-end function getFinal
-
-!}}}
-
 end module module_common
