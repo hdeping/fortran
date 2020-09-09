de4784e6689e8905c313c5355127dbc70b4c308c
黄德平(Deping Huang, Macbook Air) <xiaohengdao@gmail.com>
2020-09-09 21:13:52 +0800

    add 12_LaplaceInversion/01_version

--- math/main.f90
+++ math/main.f90
@@ -1,22 +1,6 @@
 program main
-    use module_common
+    use module_laplace
     implicit none
-    integer         :: a(n)
-    integer         :: times
 
-    a = 0
-    do i = 1,30
-        do j = n,1,-1
-            if(a(j) == 0)then
-                times    = j
-                a(times) = 1
-                exit
-            endif
-        end do
-        times = times + 1
-        do j = times,n
-            a(j) = 0
-        end do
-        print "(<n>i1)",a(:)
-    end do
+    call get_inver()
 end program main
